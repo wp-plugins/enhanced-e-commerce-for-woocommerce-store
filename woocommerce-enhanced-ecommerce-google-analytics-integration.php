d908524b1f3df534ffa5fee9af8f3bf632005c96
Tatvic <Tatvic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 11:48:43 +0000

    Version 1.0.16 Updated as per wordpress guideline
    git-svn-id: https://plugins.svn.wordpress.org/enhanced-e-commerce-for-woocommerce-store/trunk@1125282 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-enhanced-ecommerce-google-analytics-integration.php
+++ woocommerce-enhanced-ecommerce-google-analytics-integration.php
@@ -21,7 +21,7 @@
   Description: Allows Enhanced E-commerce Google Analytics tracking code to be inserted into WooCommerce store pages.
   Author: Tatvic
   Author URI: http://www.tatvic.com
-  Version: 1.0.15
+  Version: 1.0.16
  */
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
